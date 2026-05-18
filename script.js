560b19459b5b97c1496a515775857878e9994410
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-05-18 12:24:17 -0300

    Create function to play a single round
    The playRound function is responsible to display
    a message based on the winner and increment it
    score.

--- script.js
+++ script.js
@@ -16,5 +16,37 @@ function getHumanChoice() {
     return humanChoice;
 }
 
+function playRound(humanChoice, computerChoice) {
+    humanChoice = humanChoice.toLowerCase();
+    computerChoice = computerChoice.toLowerCase();
+    let winAnnouncement;
+
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        winAnnouncement = "You lose! Paper beats Rock";
+        computerScore++;
+    } else if (humanChoice === "rock" && computerChoice === "scissors") {
+        winAnnouncement = "You win! Rock beats Scissors";
+        playerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "rock") {
+        winAnnouncement = "You win! Paper beats Rock";
+        playerScore++;
+    } else if (humanChoice === "paper" && computerChoice === "scissors") {
+        winAnnouncement = "You lose! Scissors beats Paper";
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "rock") {
+        winAnnouncement = "You lose! Rock beats Scissors";
+        computerScore++;
+    } else if (humanChoice === "scissors" && computerChoice === "paper") {
+        winAnnouncement = "You win! Scissors beats Paper";
+        playerScore++;
+    } else {
+        winAnnouncement = "Draw!";
+    }
+
+    console.log(winAnnouncement);
+}
+
 let playerScore = 0;
 let computerScore = 0;
+
+playRound("Paper", "scissors");
