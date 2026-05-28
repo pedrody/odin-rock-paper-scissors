53b3b0528b74da7f3c309f9f701ba9c6b1722a02
Pedro Azevedo <91354573+pedrody@users.noreply.github.com>
2026-05-28 20:41:59 -0300

    Add play again option

--- script.js
+++ script.js
@@ -38,10 +38,27 @@ function playGame() {
 
         roundResultPara.textContent = roundResult;
     }
+
+    function resetGame() {
+        playerScore = 0;
+        computerScore = 0;
+
+        playAgainBtn.style.display = "none";
+        choiceBtns.forEach(btn => btn.style.display = "inline");
+        roundResultPara.textContent = `The first to reach ${POINTS_TO_WIN} points wins!`;
+        scorePara.textContent = `Your score: ${playerScore}  | Computer score: ${computerScore}`;
+    }
+
+    function gameOver() {
+        scorePara.textContent = (playerScore == POINTS_TO_WIN) ? "You won the game! 🎉" : "You lost the game ❌";
+        choiceBtns.forEach(btn => btn.style.display = "none");
+        playAgainBtn.style.display = "inline";
+    }
     
     let playerScore = 0;
     let computerScore = 0;
     const choiceBtns = document.querySelectorAll("#rock, #paper, #scissors");
+    const playAgainBtn = document.querySelector("#play-again");
     let roundResultPara = document.querySelector("#round-result");
     let scorePara = document.querySelector("#score");
     const POINTS_TO_WIN = 5;
@@ -52,15 +69,14 @@ function playGame() {
         playRound(e.target.id, getComputerChoice());
 
         if (playerScore == POINTS_TO_WIN || computerScore == POINTS_TO_WIN) {
-            scorePara.textContent = (playerScore == POINTS_TO_WIN) ? "You won the game! 🎉" : "You lost the game ❌";
-            
-            playerScore = 0;
-            computerScore = 0;
+            gameOver();
             
         } else {
             scorePara.textContent = `Your score: ${playerScore}  | Computer score: ${computerScore}`;
         }
     }));
+
+    playAgainBtn.addEventListener("click", resetGame);
 }
 
 playGame();
